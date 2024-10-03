3e385c046359d4f0a9a940402316ac1d1e833485
Jeffrey Keem <keemaje5@gmail.com>
2024-10-03 13:48:16 -0700

    skeleton files for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
