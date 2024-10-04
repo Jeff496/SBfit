28f953b1c0956a19326119c0612033740ccc3efb
Jeffrey Keem <keemaje5@gmail.com>
2024-10-04 16:40:47 -0700

    custom types for reducer

--- client/src/context/reducers/actionTypes.jsx
+++
@@ -1,9 +0,0 @@
-export const ACTION_TYPES = {
-  // for allWorkoutsReducer
-  SET_WORKOUTS: "SET_WORKOUT",
-  CREATE_WORKOUT: "CREATE_WORKOUT",
-  DELETE_WORKOUT: "DELETE_WORKOUT",
-
-  // for workoutReducer
-  SET_INPUTS: "SET_INPUTS",
-};
