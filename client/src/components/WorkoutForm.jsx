fbe20116f5f7252911a400c035562751e5b5b4cf
Jeffrey Keem <keemaje5@gmail.com>
2024-10-04 13:11:37 -0700

    workout form component completed, sends post req successfully

---
+++ client/src/components/WorkoutForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState("");
+  const [sets, setSets] = useState("");
+  const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { title, sets, reps };
+
+    const response = await fetch("http://localhost:3000/record", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.err);
+    }
+    if (response.ok) {
+      setTitle("");
+      setSets("");
+      setReps("");
+      setError(null);
+      console.log("new workout added", json);
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h3>Add a workout</h3>
+
+      <label>Exercise Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+
+      <label>Sets:</label>
+      <input
+        type="number"
+        onChange={(e) => setSets(e.target.value)}
+        value={sets}
+      />
+
+      <label>Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+
+      <button>Add workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
