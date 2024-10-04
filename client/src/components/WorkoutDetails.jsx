d0752a35443e9586ded606151293fc4acf1e4500
Jeffrey Keem <keemaje5@gmail.com>
2024-10-04 13:11:09 -0700

    fixed deprecated name key to title

--- client/src/components/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const WorkoutDetails = ({ workout }) => {
   return (
     <div className="workout-details">
-      <h3>{workout.name}</h3>
+      <h3>{workout.title}</h3>
       <p>Sets: {workout.sets}</p>
       <p>Reps: {workout.reps}</p>
       <p>Workout created at: {workout.createdAt || "no date created"}</p>
@@ -14,7 +14,7 @@ const WorkoutDetails = ({ workout }) => {
 WorkoutDetails.propTypes = {
   workout: PropTypes.shape({
     _id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     sets: PropTypes.number.isRequired,
     reps: PropTypes.number.isRequired,
     createdAt: PropTypes.string.isRequired,
