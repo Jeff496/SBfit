47b2aba90d4b54b8754f317612070ce9c126bb8d
Jeffrey Keem <keemaje5@gmail.com>
2024-10-08 15:54:40 -0700

    save users login status over page refreshes and load user from local storage back to react

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import { authReducer } from "../reducers/authReducer";
+import { ACTION_TYPES } from "../reducers/actionTypes";
 
 export const AuthContext = createContext();
 
@@ -8,6 +9,15 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  // check to see if user had already been logged in before, if yes update user's initial state
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch({ type: ACTION_TYPES.LOGIN, payload: user });
+    }
+  }, []);
 
   console.log("AuthContext state: ", state);
 
