d78e6cee306849046bf729335d3eb643003f9125
Jeffrey Keem <keemaje5@gmail.com>
2024-10-08 13:38:10 -0700

    controllers for user signup and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,29 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
 
-const loginUser = async (req, res) => {
-  res.json({ mssg: "user login" });
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
-const signupUser = async (req, res) => {
-  res.json({ mssg: "user signup" });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.login(email, password);
+
+  const token = createToken(user._id);
+
+  res.status(200).json({ email, token });
+});
+
+const signupUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.signup(email, password);
+
+  const token = createToken(user._id);
+
+  res.status(200).json({ email, token });
+});
 
 module.exports = { loginUser, signupUser };
