9c27edb3d09df1e2acd3eacaf4420688e8d258de
Jeffrey Keem <keemaje5@gmail.com>
2024-10-08 13:38:37 -0700

    routes for user and middleware to handle errors

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,12 @@ const { loginUser, signupUser } = require("../controllers/userController");
 const userRouter = new Router();
 
 userRouter.post("/login", loginUser);
+userRouter.post("/signup", signupUser);
 
-userRouter.post("/signuup", signupUser);
+userRouter.use((err, req, res, next) => {
+  console.log("user router error");
+  console.log(err.stack);
+  res.status(err.statusCode || 500).json({ err: err.message });
+});
 
 module.exports = userRouter;
