38fedbdaf7b9d03b0d895a93f76a412b76b98e90
Jeffrey Keem <keemaje5@gmail.com>
2024-10-08 17:52:50 -0700

    clear state when logging out so it isn't rendered before state resets to other user's workouts

--- client/src/hooks/useLogout.jsx
+++ client/src/hooks/useLogout.jsx
@@ -1,12 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useAllWorkoutsContext } from "./useAllWorkoutsContext";
 import { ACTION_TYPES } from "../reducers/actionTypes";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: allWorkoutsDispatch } = useAllWorkoutsContext();
 
   const logout = () => {
     localStorage.removeItem("user");
     dispatch({ type: ACTION_TYPES.LOGOUT });
+    // clear state when logged out
+    allWorkoutsDispatch({ type: ACTION_TYPES.SET_WORKOUTS, payload: null });
   };
 
   return { logout };
