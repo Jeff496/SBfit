3f214910fdce2a5deb96ce35baa8c6c25ee38a9e
Jeffrey Keem <keemaje5@gmail.com>
2024-10-10 18:33:05 -0700

    clean up code to be developed later

--- pythonAPI/app.py
+++ pythonAPI/app.py
@@ -14,25 +14,9 @@ CORS(app)
 def generate_graph():
     data = request.get_json()
     print(f"Data: {data}")
-    sets = [record['sets'] for record in data]
-    reps = [record['reps'] for record in data]
-    weight = [record['weight'] for record in data]
+    
 
-    plt.figure()
-    plt.plot(reps, weight, marker='o')
-    plt.title('reps and weight')
-    plt.xlabel(reps)
-    plt.ylabel(weight)
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    # Encode the image to base64
-    graph_image = base64.b64encode(buf.read()).decode('utf-8')
-    buf.close()
-
-    return jsonify({'graph': 'graphhhhhh'})
+    return jsonify({'graph': 'request sent'})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
