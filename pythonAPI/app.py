df6e3b786b7afa4a8323f9fd4508bf487891badd
Jeffrey Keem <keemaje5@gmail.com>
2024-10-11 23:41:06 -0700

    get title from workouts

--- pythonAPI/app.py
+++ pythonAPI/app.py
@@ -13,10 +13,11 @@ CORS(app)
 @app.route('/pythonAPI/graph', methods=['POST'])
 def generate_graph():
     data = request.get_json()
+    title = [record['title'] for record in data]
     print(f"Data: {data}")
     
 
-    return jsonify({'graph': 'request sent'})
+    return jsonify({'title': title})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
