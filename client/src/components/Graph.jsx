090da401b5e99df1b95a80d11f4b07a22d48c0fd
Jeffrey Keem <keemaje5@gmail.com>
2024-10-14 23:57:06 -0700

    progress on search function for graph component

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,7 +1,10 @@
 import { useAllWorkoutsContext } from "../hooks/useAllWorkoutsContext";
+import { useState, useEffect } from "react";
 
 const Graph = () => {
   const { workouts } = useAllWorkoutsContext();
+  const [query, setQuery] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const handleSubmit = (e) => {
     // access all workouts with the chosen title from user workouts
@@ -10,12 +13,40 @@ const Graph = () => {
     );
   };
 
+  const handleInput = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    const filteredWorkouts = workouts.filter((workout) =>
+      workout.title.toLowerCase().startsWith(query.toLowerCase())
+    );
+
+    console.log("filtwork", filteredWorkouts);
+
+    setFilteredResults(filteredWorkouts);
+  }, [query, workouts]);
+
   return (
     <>
       <div>
         {/* button */}
         <form onSubmit={handleSubmit}>
           {/* search function that shows you titles of workouts you have */}
+          <input
+            type="text"
+            placeholder="Search workouts..."
+            value={query}
+            onChange={handleInput}
+          />
+          <div className="searchResults">
+            {console.log("filt", filteredResults)}
+            <ul>
+              {filteredResults.map((workout, index) => (
+                <li key={index}>{workout.title}</li>
+              ))}
+            </ul>
+          </div>
           <button>Generate Graph</button>
         </form>
       </div>
