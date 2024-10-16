e65c4fb867fba61917c87b845e57551808b8b767
Jeffrey Keem <keemaje5@gmail.com>
2024-10-15 18:53:44 -0700

    format data into arrays for graphing and send as response to express backend

--- pythonAPI/app.py
+++ pythonAPI/app.py
@@ -1,44 +1,52 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 from flask_cors import CORS
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
 from datetime import datetime
-import os
 import io
-import base64
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
+matplotlib.use('Agg')
+
 
 @app.route('/pythonAPI/graph', methods=['POST'])
 def generate_graph():
     data = request.get_json()
-    # used for selection, get data only for exercises that have that title
-    title = [record['title'] for record in data]
 
-    # todo: select time, reps, and weight only from records that have the title and parse time
-    time = [record['updatedAt'] for record in data]
+    # Get workout title for graph name
+    title = f'{data[0]['title']} progress'
+    
+    # final time used
+    isoTime = [record['updatedAt'] for record in data]
+    time = [datetime.fromisoformat(record.replace("Z", "+00:00")) for record in isoTime]
+    # print(f'time: {time}')
+
+    #retrieve weight
     reps = [record['reps'] for record in data]
-    weight = [record['weight'] for record in data]
-    print(f"Data: {data}")
+    weights = [record['weight'] for record in data]
     
     # formula for one rep max used for tracking progress
     # final array for weight
-    oneRepMax = weight / ( 1.0278 - 0.0278 * reps )
+    oneRepMax = [weight / (1.0278 - 0.0278 * rep) for weight, rep in zip(weights, reps)]
+    # print(f'orm', oneRepMax)
 
-    # array to sync one rep max and time of workout
+    plt.figure(figsize=(10, 5))
+    plt.plot(time, oneRepMax, marker='o')
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Calculated One Rep Max')
+    plt.xticks(rotation=45)
+    plt.grid()
 
-    workouts = np.column_stack((time, oneRepMax))
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
 
-    plt.figure()
-    plt.plot(x_data, oneRepMax, marker='o')
-    plt.title('Line Graph')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-
-    return jsonify({'title': title})
+    return send_file(img, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
