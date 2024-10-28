a426afe7ded18a011b409780f41329f79f8253d2
Jeffrey Keem <keemaje5@gmail.com>
2024-10-27 23:54:48 -0700

    styling and formatting for user auth methods

--- client/src/pages/UserAuth.jsx
+++ client/src/pages/UserAuth.jsx
@@ -1,11 +1,13 @@
 import Login from "../components/Login";
 import Signup from "../components/Signup";
+import GoogleOAuth from "../components/GoogleOAuth";
 
 const UserAuth = () => {
   return (
     <>
       <Signup />
       <Login />
+      <GoogleOAuth />
     </>
   );
 };
